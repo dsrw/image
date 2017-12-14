c786ba2da418ed125285a874928b60dae2876698
Nalin Dahyabhai <nalin@redhat.com>
2017-12-14 16:52:47 -0500

    copy.Image(): copy lists if MultipleImages is set
    Add a MultipleImages flag to copy.Options, and if the source for a copy
    operation contains multiple images, copy all of the images if we can.
    If we can't, but we were told to, return an error.
    
    Signed-off-by: Nalin Dahyabhai <nalin@redhat.com>

--- copy/manifest_test.go
+++ copy/manifest_test.go
@@ -196,6 +196,8 @@ func TestIsMultiImage(t *testing.T) {
 	}{
 		{manifest.DockerV2ListMediaType, true},
 		{manifest.DockerV2Schema2MediaType, false},
+		{v1.MediaTypeImageManifest, false},
+		{v1.MediaTypeImageIndex, true},
 	} {
 		src := fakeImageSource(c.mt)
 		res, err := isMultiImage(src)
